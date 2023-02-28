87b5298c3fa9f3da0d0a0df579d50158efc73839
GameCom28 <romain.martin28260@gmail.com>
2023-02-28 10:12:06 +0100

    Exercice1-4

---
+++ exercice1-4.ps1
@@ -0,0 +1,13 @@
+﻿#========================================================================
+#NAME: 
+#AUTHOR: GameCom28
+#DATE: 28/02/2023
+#
+#VERSION 1.0
+#COMMENTS:
+#
+#Requires -Version 2.0
+#========================================================================
+
+$Processus = Read-Host "Entrez votre processus"
+Get-Process $Processus | select ProcessName,Id,Description | Out-GridView
