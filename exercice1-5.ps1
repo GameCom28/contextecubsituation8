1205cf256fc03f4b21604797d6eecd21acf065dc
GameCom28 <romain.martin28260@gmail.com>
2023-02-28 10:30:18 +0100

    Exercice1-5

---
+++ exercice1-5.ps1
@@ -0,0 +1,16 @@
+﻿#========================================================================
+#NAME: 
+#AUTHOR: GameCom28
+#DATE: 28/02/2023
+#
+#VERSION 1.0
+#COMMENTS:
+#
+#Requires -Version 2.0
+#========================================================================
+function InfoProcessus($Power){
+Get-Process $Power | select ProcessName,Id,Description | Out-GridView
+}
+
+$input = Read-Host "Entrez votre processus"
+InfoProcessus $input
