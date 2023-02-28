bc292e309f5d0500bed6e6905a533702e8e01240
GameCom28 <romain.martin28260@gmail.com>
2023-02-28 10:49:53 +0100

    Exercice1-6

---
+++ exercice1-6.ps1
@@ -0,0 +1,18 @@
+﻿#========================================================================
+#NAME: 
+#AUTHOR: GameCom28
+#DATE: 28/02/2023
+#
+#VERSION 1.0
+#COMMENTS:
+#
+#Requires -Version 2.0
+#========================================================================
+#Vars
+$path = $env:HOMEDRIVE+$env:HOMEPATH+"\Documents"
+function InfoProcessus($Power){
+Get-Process *$Power* | select ProcessName,Id,Description | Export-Csv -Path $path"\sauvegarde.csv" -NoTypeInformation
+Get-Process *$Power* | select ProcessName,Id,Description | Out-File $path"\sauvegarde.txt"
+}
+$input = Read-Host "Entrez votre processus"
+InfoProcessus $input
