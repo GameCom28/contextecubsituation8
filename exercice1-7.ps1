89a796d874d41eb3eb691befd64d54472a7e4a80
GameCom28 <romain.martin28260@gmail.com>
2023-02-28 11:07:14 +0100

    Exercice1-7

---
+++ exercice1-7.ps1
@@ -0,0 +1,17 @@
+﻿#========================================================================
+#NAME: 
+#AUTHOR: GameCom28
+#DATE: 28/02/2023
+#
+#VERSION 1.0
+#COMMENTS:
+#
+#Requires -Version 2.0
+#========================================================================
+#Vars
+$path = $env:HOMEDRIVE+$env:HOMEPATH+"\Documents"
+
+Get-Service | select Name,DisplayName,Status | Export-Csv -Path $path"\services.csv" -NoTypeInformation
+
+Write-Host "Fichier Exporté dans"$path
+
