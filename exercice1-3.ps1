50c36e57fd25e5d58f3fc10d60cee1f65f52b0d5
GameCom28 <romain.martin28260@gmail.com>
2023-02-28 09:59:27 +0100

    Exercice1-3

---
+++ exercice1-3.ps1
@@ -0,0 +1,12 @@
+﻿#========================================================================
+#NAME: 
+#AUTHOR: GameCom28
+#DATE: 28/02/2023
+#
+#VERSION 1.0
+#COMMENTS:
+#
+#Requires -Version 2.0
+#========================================================================
+
+Get-Process svchost | select ProcessName,Id,Description | Out-GridView
